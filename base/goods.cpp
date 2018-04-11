6596b9e234ab35e560d5147fb9ca2a29512cf749
nyaachen <killyouabc@live.cn>
2018-04-11 14:11:47 +0800

    fix namespace

--- base/goods.cpp
+++ base/goods.cpp
@@ -2,23 +2,22 @@
 #include <string>
 #include <vector>
 
-
+// TODO 优化底层的实现方法
 
 class Goods {
 	private :
-		string main_attribute {};
-		vector<string> attribute_list {};
+		std::string main_attribute {};
+		std::vector<std::string> attribute_list {};
 	public :
 		Goods() = default;
-		explicit Goods(string s) : Goods() {
-			isstream is(s);
+		explicit Goods(std::string s) : Goods() {
+			std::isstream is(s);
 			is >> main_attribute;
-			string temp;
+			std::string temp;
 			while (is >> temp)
 				attribute_list.emplace(temp);
 		}
 		Goods(Goods &goods) = delete;
-		Goods * set_main_attribute(string s) {main_attribute = s; return this;}
+		Goods * set_main_attribute(const string &s) {main_attribute = s; return this;}
 		const string & get_main_attribute() {return main_attribute;}
 }
-
