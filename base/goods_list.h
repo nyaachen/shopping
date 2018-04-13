8f4592ed7eb4e2a9cdaa60d3253aff292e9dc719
nyaachen <killyouabc@live.cn>
2018-04-13 15:21:37 +0800

    update

--- base/goods_list.h
+++ base/goods_list.h
@@ -10,12 +10,12 @@ class Goods_list {
   private :
     std::vector<Goods> goods_list;
   public :
-    Stock();
-    explicit Stock(const std::vector<Goods> &source_list);
+    Goods_list();
+    explicit Goods_list(const std::vector<Goods> &source_list);
     Goods_list find_by_id(const std::string &id);
     Goods_list find_by_name(const std::string &name);
     Goods_list find_by_brand(const std::string &brand);
-    Stock & sell_cart();
+    //Stock & sell_cart();
     decltype(goods_list.cbegin()) cbegin();
     decltype(goods_list.cend()) cend();
 
@@ -23,6 +23,7 @@ class Goods_list {
     bool is_empty() const;
 
     bool all_goods_is_positive() const;
+    void remove_non_positive();
 
     Goods_list operator+(const Goods_list &another) const;
     Goods_list operator-(const Goods_list &another) const;
--- base/manager.h
+++ base/manager.h
@@ -10,15 +10,39 @@
 #include "user.h"
 
 class Manager {
+  constexpr User defaultUser("游客", "请登录");
 private:
   std::vector<User> user_list;
   std::vector<SuperUser> admin_list;
   goods_list stock;
+  bool init_stock(std::string filename) {}
+  bool init_user_list(std::string filename) {}
+  bool init_superuser_list(std::string filename) {}
 public:
   explicit Manager(const std::string filename) {
     // TODO finish initializing library from file
   }
-  
+  SuperUser &superuser_login(std::string uname, std::string pwd) {
+    auto iter = std::find_if(admin_list.begin(), admin_list.end(), [uname] (const SuperUser &u) -> bool {return u.get_username() == uname;});
+    if ((iter != user_list.end()) and (iter->verify(pwd)))
+      return *iter;
+    //FIX report this error correctly
+    throw "Invalid Argument";
+  }
+  bool superuser_remove_item(const SuperUser &u, const std::string &id) {}
+  bool superuser_remove_item(const SuperUser &u, const Goods &item) {}
+  bool superuser_add_item(const SuperUser &u, const Goods &item) {}
+  bool superuser_modify_item(const SuperUser &u, const Goods &item) {}
+  std::string superuser_view_sell_list(const SuperUser &u) {}
+  void superuser_view_item(const SuperUser &u) {} //FIX
+
+  User &login(std::string uname, std::string pwd) {
+    auto iter = std::find_if(user_list.begin(), user_list.end(), [uname] (const User &u) -> bool {return u.get_username() == uname;});
+    if ((iter != user_list.end()) and (iter->verify(pwd)))
+      return *iter;
+    return defaultUser;
+  }
+  void user_view_item(const User &u, ) {} //FIX
 };
 
 
